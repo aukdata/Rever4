0c9d24989e3105ee7d17572ca0ca04473ee7c307
takuma <takuma.y0524@gmail.com>
2017-08-28 17:17:11 +0900

    Mobile Support

--- js/disks.js
+++ js/disks.js
@@ -8,7 +8,7 @@ let disks = new Array(5);
 disks[DISK_NULL] = {id: DISK_NULL, color: "", name: ""};
 disks[DISK_RED] = {id: DISK_RED, color: "#B53E2F", name: "red"};
 disks[DISK_BLUE] = {id: DISK_BLUE, color: "#26547C", name: "blue"};
-disks[DISK_YELLOW] = {id: DISK_YELLOW, color: "#FFD87F", name: "yellow"};
+disks[DISK_YELLOW] = {id: DISK_YELLOW, color: "#FFC744", name: "yellow"};
 disks[DISK_GREEN] = {id: DISK_GREEN, color: "#2C7043", name: "green"};
 
 document.addEventListener("DOMContentLoaded", function(e) {
